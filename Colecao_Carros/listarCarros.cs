8677ebe5e95c885732c27b318f4bd99c5a62dc48
Guilherme <guilhermeluiz.franco.dev@gmail.com>
2025-12-27 17:51:19 -0200

    Segundo commit do projeto: VALIDAÇÃO CASO OBJETO PROCURADO NÃO EXISTA + ELIMINAÇÃO DE BLOCOS DE CÓDIGO

--- Colecao_Carros/listarCarros.cs
+++ Colecao_Carros/listarCarros.cs
@@ -2,6 +2,73 @@ using MySqlConnector;
 
 public class ListarCarros
 {
+
+  public static void functioString(string sql, string variavel1, string variavel2)
+  {
+    var conn = dbConnection.ConnectionState();
+    using(var cmd = new MySqlCommand(sql, conn))
+    {
+      cmd.Parameters.AddWithValue($"@{variavel1}", variavel2);
+      using (MySqlDataReader reader = cmd.ExecuteReader()){
+         if(reader.HasRows == false){
+            Console.WriteLine("Nenhum carro encontrado.\n");
+          
+         }
+          else
+          {
+            while(reader.Read()){
+              Console.WriteLine($"ID: {reader["id"]} | NOME: {reader["nome"]} | MARCA: {reader["marca"]} | ANO: {reader["ano"]} | PRECO: {reader["preco"]}");
+          }
+        }
+     }
+       
+    }
+  }
+  public static void functionInt(string sql, string variavel1, int variavel2, string variavel3, int variavel4)
+  {
+    var conn = dbConnection.ConnectionState();
+    using(var cmd = new MySqlCommand(sql, conn))
+    {
+      cmd.Parameters.AddWithValue($"@{variavel1}", variavel2);
+      cmd.Parameters.AddWithValue($"@{variavel3}", variavel4);
+      using (MySqlDataReader reader = cmd.ExecuteReader()){
+         if(reader.HasRows == false){
+            Console.WriteLine("Nenhum carro encontrado.\n");
+          
+         }
+          else
+          {
+            while(reader.Read()){
+              Console.WriteLine($"ID: {reader["id"]} | NOME: {reader["nome"]} | MARCA: {reader["marca"]} | ANO: {reader["ano"]} | PRECO: {reader["preco"]}");
+          }
+        }
+     }
+      
+    }
+  }
+
+   public static void functionFloat(string sql, string variavel1, float variavel2, string variavel3, float variavel4)
+  {
+    var conn = dbConnection.ConnectionState();
+    using(var cmd = new MySqlCommand(sql, conn))
+    {
+      cmd.Parameters.AddWithValue($"@{variavel1}", variavel2);
+      cmd.Parameters.AddWithValue($"@{variavel3}", variavel4);
+      using (MySqlDataReader reader = cmd.ExecuteReader()){
+         if(reader.HasRows == false){
+            Console.WriteLine("Nenhum carro encontrado.\n");
+          
+         }
+          else
+          {
+            while(reader.Read()){
+              Console.WriteLine($"ID: {reader["id"]} | NOME: {reader["nome"]} | MARCA: {reader["marca"]} | ANO: {reader["ano"]} | PRECO: {reader["preco"]}");
+          }
+        }
+     }
+       
+    }
+  }
   public static void Listar()
   {
     string valor;
@@ -17,20 +84,11 @@ public class ListarCarros
       carro.Nome = Console.ReadLine();
       
       string sql = @"SELECT * FROM carros WHERE nome = @nome;";
+      string variavel1 = "nome";
+      string variavel2 = carro.Nome;
+
+      functioString(sql, variavel1, variavel2);
       
-      var conn = dbConnection.ConnectionState();
-      using(var cmd = new MySqlCommand(sql, conn))
-      {
-        cmd.Parameters.AddWithValue(@"nome", carro.Nome);
-        
-        using (var reader = cmd.ExecuteReader()){
-            
-            while (reader.Read()){
-              Console.WriteLine($"ID: {reader["id"]} | NOME: {reader["nome"]} | MARCA: {reader["marca"]} | ANO: {reader["ano"]} | PRECO: {reader["preco"]}");
-            }
-        }
-      }
-       conn.Dispose();
     }
     else if(valor == "marca")
     {
@@ -38,18 +96,10 @@ public class ListarCarros
       carro.Marca = Console.ReadLine();
       
       string sql = @"SELECT * FROM carros WHERE marca = @marca;";
+      string variavel1 = "marca";
+      string variavel2 = carro.Marca;
       
-      var conn = dbConnection.ConnectionState();
-      using(var cmd = new MySqlCommand(sql, conn))
-      {
-        cmd.Parameters.AddWithValue(@"marca", carro.Marca);
-        using (var reader = cmd.ExecuteReader()){
-        while(reader.Read()){
-        Console.WriteLine($"ID: {reader["id"]} | NOME: {reader["nome"]} | MARCA: {reader["marca"]} | ANO: {reader["ano"]} | PRECO: {reader["preco"]}");
-         }
-        }
-     }
-       conn.Dispose();
+      functioString(sql, variavel1, variavel2);
     }
      else if(valor == "ano")
      {
@@ -65,18 +115,12 @@ public class ListarCarros
         carro.Ano = int.Parse(Console.ReadLine());
       
         string sql1 = @"SELECT * FROM carros WHERE ano = @ano;";
-      
-        var conn1 = dbConnection.ConnectionState();
-        using(var cmd = new MySqlCommand(sql1, conn1))
-         {
-           cmd.Parameters.AddWithValue(@"ano", carro.Ano);
-           using (var reader = cmd.ExecuteReader()){
-           while(reader.Read()){
-           Console.WriteLine($"ID: {reader["id"]} | NOME: {reader["nome"]} | MARCA: {reader["marca"]} | ANO: {reader["ano"]} | PRECO: {reader["preco"]}");
-         }
-           }
-         }
-         conn1.Dispose();
+        string variavel1 = "ano"; 
+        string variavel2 = carro.Ano.ToString();
+
+        functioString(sql1, variavel1, variavel2);
+
+       
          break;
        
         case 2:
@@ -88,19 +132,15 @@ public class ListarCarros
          anoDois = int.Parse(Console.ReadLine());
       
          string sql2 = @"SELECT * FROM carros WHERE ano <= @anoInicial AND ano >= @anoFinal;";
-      
-         var conn2 = dbConnection.ConnectionState();
-         using(var cmd = new MySqlCommand(sql2, conn2))
-         {
-           cmd.Parameters.AddWithValue(@"anoInicial", anoUm);
-           cmd.Parameters.AddWithValue(@"anoFinal", anoDois);
-           using (var reader = cmd.ExecuteReader()){
-           while(reader.Read()){
-           Console.WriteLine($"ID: {reader["id"]} | NOME: {reader["nome"]} | MARCA: {reader["marca"]} | ANO: {reader["ano"]} | PRECO: {reader["preco"]}");
-         }
-           }
-         }
-         conn2.Dispose();
+         
+         string variavel3 = "anoInicial";
+         int variavel4 = anoUm;  
+         string variavel5 = "anoFinal";
+         int variavel6 = anoDois;
+
+         functionInt(sql2, variavel3, variavel4, variavel5, variavel6);
+        
+         
          break;
        }
     }else if(valor == "preco")
@@ -112,25 +152,17 @@ public class ListarCarros
       
       switch(op)
             {
-                
-            
          case 1:
+
         Console.WriteLine("Insira o preco do carro:\n");
         carro.Preco = float.Parse(Console.ReadLine());
       
         string sql1 = @"SELECT * FROM carros WHERE preco = @preco;";
       
-        var conn1 = dbConnection.ConnectionState();
-        using(var cmd = new MySqlCommand(sql1, conn1))
-         {
-           cmd.Parameters.AddWithValue(@"preco", carro.Preco);
-           using (var reader = cmd.ExecuteReader()){
-           while(reader.Read()){
-           Console.WriteLine($"ID: {reader["id"]} | NOME: {reader["nome"]} | MARCA: {reader["marca"]} | ANO: {reader["ano"]} | PRECO: {reader["preco"]}");
-         }
-           }
-         }
-         conn1.Dispose();
+        string variavel1 = "preco";
+        string variavel2 = carro.Preco.ToString();
+
+        functioString(sql1, variavel1, variavel2);
          break;
        
         case 2:
@@ -142,19 +174,13 @@ public class ListarCarros
          precoDois = float.Parse(Console.ReadLine());
       
          string sql2 = @"SELECT * FROM carros WHERE preco <= @precoInicial AND preco >= @precoFinal;";
-      
-         var conn2 = dbConnection.ConnectionState();
-         using(var cmd = new MySqlCommand(sql2, conn2))
-         {
-           cmd.Parameters.AddWithValue(@"precoInicial", precoUm);
-           cmd.Parameters.AddWithValue(@"precoFinal", precoDois);
-           using (var reader = cmd.ExecuteReader()){
-           while(reader.Read()){
-           Console.WriteLine($"ID: {reader["id"]} | NOME: {reader["nome"]} | MARCA: {reader["marca"]} | ANO: {reader["ano"]} | PRECO: {reader["preco"]}");
-         }
-           }
-         }
-         conn2.Dispose();
+         string variavel3 = "precoInicial";
+         float variavel4 = precoUm;  
+         string variavel5 = "precoFinal";
+         float variavel6 = precoDois;
+
+         functionFloat(sql2, variavel3, variavel4, variavel5, variavel6);
+        
          break;
        }
         }
